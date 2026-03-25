50c9bd15a9c1276984273409cde24d49a726db85
quangson2809 <quangson25555@example.com>
2026-03-25 17:18:40 +0700

    initial commit

---
+++ src/main/java/com/example/the_cheaper/TheCheaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_cheaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCheaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCheaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/the_cheaper/TheCheaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_cheaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCheaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
