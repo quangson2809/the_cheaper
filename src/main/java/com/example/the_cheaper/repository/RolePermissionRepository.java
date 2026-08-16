c97e8cef2eb850068df90d4d99d6c8876668334c
QuangSon2809 <dangquangsontk2005@gmail.com>
2026-08-16 23:33:39 +0700

    feat(rbac): add role permission repository

---
+++ src/main/java/com/example/the_cheaper/repository/RolePermissionRepository.java
@@ -0,0 +1,14 @@
+package com.example.the_cheaper.repository;
+
+import com.example.the_cheaper.entity.RolePermissionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RolePermissionRepository extends JpaRepository<RolePermissionEntity, Long> {
+    boolean existsByPermissionId(Long permissionId);
+
+    boolean existsByRoleIdAndPermissionId(Long roleId, Long permissionId);
+
+    List<RolePermissionEntity> findAllByRoleId(Long roleId);
+}
