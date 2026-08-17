62cae7d972908b9a7ea64842f61566822123debe
QuangSon2809 <dangquangsontk2005@gmail.com>
2026-08-17 13:27:36 +0700

    feat(rbac): support role permission revoke lookup

--- src/main/java/com/example/the_cheaper/repository/RolePermissionRepository.java
+++ src/main/java/com/example/the_cheaper/repository/RolePermissionRepository.java
@@ -4,6 +4,7 @@ import com.example.the_cheaper.entity.RolePermissionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RolePermissionRepository extends JpaRepository<RolePermissionEntity, Long> {
     boolean existsByPermissionId(Long permissionId);
@@ -11,4 +12,6 @@ public interface RolePermissionRepository extends JpaRepository<RolePermissionEn
     boolean existsByRoleIdAndPermissionId(Long roleId, Long permissionId);
 
     List<RolePermissionEntity> findAllByRoleId(Long roleId);
+
+    Optional<RolePermissionEntity> findByRoleIdAndPermissionId(Long roleId, Long permissionId);
 }
